5c78530b79468441d69b42d151711ada112a1801
Marcin Kacperski <kacperski81@gmail.com>
2023-02-09 14:26:51 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,34 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const sytles = {
+    background: {
+        backgroundColor: 'green'
+    }
+
+}
+
 function Header() {
     return (
-        <h1>Header Component</h1>
+        <>
+            <Navbar collapseOnSelect sticky="top" expand="md" style={sytles.background}>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav variant="pills">
+                        <Nav.Item>
+                            <Nav.Link>Home</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>Projects</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>Contact</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
     )
 }
 
